1a9d2a5e32069b0ec1833657b7804733a4f33f1d
Teslimat <teslimatadeyemi@gmail.com>
2024-05-02 14:30:31 +0100

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 function refreshWeather(response){
     let cityElement = document.querySelector("#weather-app-city");
-    cityElement.innerHTML = response.data.city
-
     let temperatureElement = document.querySelector("#weather-app-temp");
     let temperature = response.data.temperature.current;
-    temperatureElement.innerHTML = Math.round(temperature)
+    let descriptionElement = document.querySelector("#weather-description");
+    let humidityElement = document.querySelector("#weather-humidity");
+    let windElement = document.querySelector("#weather-wind-speed");
+    let timeElement = document.querySelector("#weather-time");
+    let date = new Date(response.data.time *1000)
 
-    console.log(response.data.temperature.current)
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature)
+    descriptionElement.innerHTML = response.data.condition.description
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`
+    windElement.innerHTML = `${response.data.wind.speed}Km/h`
+    timeElement.innerHTML = formatDate(date)
+  
+}
+
+function formatDate(date){
+    
+    let minutes = date.getMinutes()
+    let hours = date.getHours()
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`
+    }
+
+      if (hours < 10) {
+        hours = `0${hours}`;
+      }
+
+    return `${day} ${hours}:${minutes}`
 }
 
 function searchCity(city){
